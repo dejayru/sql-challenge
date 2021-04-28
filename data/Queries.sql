9b28ce909e2a32601bd86e285d260f33bd7564d6
dejayru <rkpmobile@gmail.com>
2021-04-28 19:02:21 -0400

    hire date in 1986

--- data/Queries.sql
+++ data/Queries.sql
@@ -7,4 +7,11 @@ FROM employees AS e
 LEFT JOIN salaries AS s
 ON e.emp_no = s.emp_no;
 
+-- Get first & last name, hire date in 1986
+
+SELECT first_name, last_name, hire_date
+FROM employees
+WHERE
+hire_date >= '1986-01-01'
+AND hire_date <= '1986-12-31';
 
