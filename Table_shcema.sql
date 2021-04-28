62185592adbd82de54435b42682f24d64232bc8a
dejayru <rkpmobile@gmail.com>
2021-04-28 17:21:02 -0400

    added tables

--- Table_shcema.sql
+++ Table_shcema.sql
@@ -13,3 +13,34 @@ CREATE TABLE departments (
 );
 
 select * from departments
+
+-- employees table
+
+DROP TABLE employees
+
+CREATE TABLE employees (
+	emp_no INT NOT NULL,
+	birth_date DATE NOT NULL,
+	first_name VARCHAR(20) NOT NULL,
+	last_name VARCHAR(20) NOT NULL,
+	gender VARCHAR(2) NOT NULL,
+	hire_date DATE NOT NULL,
+	PRIMARY KEY (emp_no)
+);
+
+select * from employees
+
+-- Department Employee table
+
+DROP TABLE dept_emp
+
+CREATE TABLE dept_emp (
+	emp_no INT NOT NULL,
+	dept_no VARCHAR(10) NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+    FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+    FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
+);
+
+select * from dept_emp
