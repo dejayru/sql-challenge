8e5cfdb730fb8ff4b5b900575533eb5176ef0b8c
dejayru <rkpmobile@gmail.com>
2021-04-28 17:26:31 -0400

    Titles Table

--- Table_shcema.sql
+++ Table_shcema.sql
@@ -73,3 +73,17 @@ emp_no BIGINT NOT NULL,
 );
 
 select * from salaries
+
+-- Titles table
+
+DROP TABLE titles
+
+CREATE TABLE titles (
+	emp_no INT NOT NULL,
+	title VARCHAR(20) NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+	FOREIGN KEY(emp_no) REFERENCES employees(emp_no)
+);
+
+select * from titles
