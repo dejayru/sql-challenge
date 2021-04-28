bce6e7d07f574a2986d579f3702642f3edffc298
dejayru <rkpmobile@gmail.com>
2021-04-28 17:22:34 -0400

    dept managers table

--- Table_shcema.sql
+++ Table_shcema.sql
@@ -44,3 +44,18 @@ CREATE TABLE dept_emp (
 );
 
 select * from dept_emp
+
+-- Department Managers table
+
+DROP TABLE dept_managers
+
+CREATE TABLE dept_managers (
+ dept_no VARCHAR (10),
+ emp_no INT,
+ from_date DATE NOT NULL,
+ to_date DATE NOT NULL,
+ FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+ FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+);
+
+select * from dept_managers
