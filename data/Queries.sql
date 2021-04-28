94d2ca3953c349c5cbeddff7ed612f8eef388745
dejayru <rkpmobile@gmail.com>
2021-04-28 19:17:48 -0400

    Sales & Development Department

--- data/Queries.sql
+++ data/Queries.sql
@@ -42,6 +42,14 @@ JOIN employees ON dept_emp.emp_no = employees.emp_no
 JOIN departments ON dept_emp.dept_no = departments.dept_no
 WHERE departments.dept_name = 'Sales';
 
+-- List all employees in the Sale & Development departments, include employee number, last & first name, & department name.
+SELECT dept_emp.emp_no, employees.last_name, employees.first_name, departments.dept_name
+FROM dept_emp
+JOIN employees ON dept_emp.emp_no = employees.emp_no
+JOIN departments ON dept_emp.dept_no = departments.dept_no
+WHERE
+departments.dept_name = 'Sales'
+OR departments.dept_name = 'Development';
 
 
 
